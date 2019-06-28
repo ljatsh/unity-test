c23d7a7d3ff77d8707849e16513a6780f353211c
ljatsh <longjun.wang@qq.com>
2019-06-28 22:52:57 +0800

    tutorial - graph

--- tutorial/Assets/Scripts/Graph.cs
+++ tutorial/Assets/Scripts/Graph.cs
@@ -1,15 +1,33 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+// TODO
+// 1. Cannot assign x,y,z of localPosition
+// 2. Attribute
 
 public class Graph : MonoBehaviour {
   public Transform sample;
 
+  public Transform root;
+
+  [Range(10, 100)]
+  public Int32 resolution = 10;
+
+  private const Single showRangeX = 6.0f;
+
   void Awake() {
-    for (int i=1; i<=10; i++) {
-      var tf = Instantiate(sample);
-      tf.localPosition =  Vector3.right * i;
-      tf.localScale = Vector3.one / 5.0f;
+    Single step = showRangeX / resolution;
+    Vector3 scale = Vector3.one * step;
+
+    Vector3 position;
+    for (int i=0; i<resolution; i++) {
+      var tf = Instantiate(sample, root);
+      position = Vector3.right * (-showRangeX/2 + step * i);
+      position.y = position.x * position.x;
+      tf.localPosition = position;
+      tf.localScale = scale;
     }
   }
 
