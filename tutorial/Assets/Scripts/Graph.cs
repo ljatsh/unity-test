0d1a74ecc3d0dec350ff77c882a062e6044d1fbc
lj@sh <longjun.wang@qq.com>
2019-06-28 18:35:23 +0800

    tutorial - graph ui

---
+++ tutorial/Assets/Scripts/Graph.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour {
+  public Transform sample;
+
+  void Awake() {
+    for (int i=1; i<=10; i++) {
+      var tf = Instantiate(sample);
+      tf.localPosition =  Vector3.right * i;
+      tf.localScale = Vector3.one / 5.0f;
+    }
+  }
+
+  // Start is called before the first frame update
+  void Start() {
+      
+  }
+
+  // Update is called once per frame
+  void Update() {
+      
+  }
+}
