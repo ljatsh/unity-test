71fe20f79f8153dd1522fe9239e1f4f5792c9e41
ljatsh <longjun.wang@qq.com>
2019-06-29 09:54:37 +0800

    tutorial - animate graph

--- tutorial/Assets/Scripts/Surface.cs
+++ tutorial/Assets/Scripts/Surface.cs
@@ -3,13 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// TODO
-// 1. Cannot assign x,y,z of localPosition
-// 2. Attribute
-// 3. Array
-// 4. Unity.Mathf and Time
-
-public class Graph : MonoBehaviour {
+public class Surface : MonoBehaviour {
   public Transform sample;
 
   public Transform root;
@@ -27,26 +21,32 @@ public class Graph : MonoBehaviour {
   private const Single showRangeX = 6.0f;
   private Transform[] points;
 
-  private delegate float MappingFunction(float x, float t);
+  private delegate float MappingFunction(float x, float z, float t);
   private MappingFunction[] functions = new MappingFunction[] {
     SineFunction,
     MultiSineFunction
   };
 
   void Awake() {
-    points = new Transform[resolution];
+    points = new Transform[resolution * resolution];
 
     Single step = showRangeX / resolution;
     Vector3 scale = Vector3.one * step;
 
     Vector3 position;
+    float z;
     for (int i=0; i<resolution; i++) {
-      var cp = Instantiate(sample, root);
-      position = Vector3.right * (-showRangeX/2 + step * i);
-      cp.localPosition = position;
-      cp.localScale = scale;
+      z = -showRangeX/2 + step * i;
 
-      points[i] = cp;
+      for (int j=0; j<resolution; j++) {
+        var cp = Instantiate(sample, root);
+        position = Vector3.right * (-showRangeX/2 + step * j);
+        position.z = z;
+        cp.localPosition = position;
+        cp.localScale = scale;
+
+        points[i * resolution + j] = cp;
+      }
     }
   }
 
@@ -63,16 +63,16 @@ public class Graph : MonoBehaviour {
 
     foreach (var cp in points) {
       point = cp.localPosition;
-      point.y = mapping(point.x, Time.time);
+      point.y = mapping(point.x, point.z, Time.time);
       cp.localPosition = point;
     }
   }
 
-  static float SineFunction (float x, float t) {
+  static float SineFunction (float x, float z, float t) {
     return Mathf.Sin(Mathf.PI * (x + t));
   }
 
-  static float MultiSineFunction (float x, float t) {
+  static float MultiSineFunction (float x, float z, float t) {
     float y = Mathf.Sin(Mathf.PI * (x + t));
     y += Mathf.Sin(2f * Mathf.PI * (x + 2f * t)) / 2f;
     y *= 2f / 3f;
