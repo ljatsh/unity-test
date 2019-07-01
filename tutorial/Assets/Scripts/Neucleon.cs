4012fbba931bce98d0423e5889e60c7d04eccecf
lj@sh <longjun.wang@qq.com>
2019-07-01 21:00:20 +0800

    tutorial - performance 1

---
+++ tutorial/Assets/Scripts/Neucleon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Neucleon : MonoBehaviour {
+  public float attaractionForce;
+
+  private Rigidbody body;
+
+  void Awake() {
+    body = gameObject.GetComponent<Rigidbody>();
+  }
+
+  // Start is called before the first frame update
+  void Start() {
+    
+  }
+
+  // Update is called once per frame
+  void Update(){
+      
+  }
+
+  void FixedUpdate() {
+    body.AddForce(transform.localPosition * -attaractionForce);
+  }
+}
