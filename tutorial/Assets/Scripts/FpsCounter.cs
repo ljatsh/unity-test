390279b70c03aa28687c68a1b78717ba497c8101
ljatsh <longjun.wang@qq.com>
2019-07-02 23:31:48 +0800

    tutorial - performance 3

---
+++ tutorial/Assets/Scripts/FpsCounter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FpsCounter : MonoBehaviour {
+  public int FPSAverage { get; private set; }
+  public int FPSHighest { get; private set; }
+  public int FPSLowest { get; private set; }
+
+  public int frameRange;
+  
+  private int[] fpsBuffer;
+  private int fpsBufferIndex;
+
+  void Awake() {
+    FPSLowest = 100;
+    fpsBuffer = new int[frameRange];
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    int fps = (int)(1f / Time.unscaledDeltaTime);
+
+    fpsBuffer[fpsBufferIndex++] = (int)(1f / Time.unscaledDeltaTime);
+    if (fpsBufferIndex >= frameRange) {
+      fpsBufferIndex = 0;
+    }
+
+    int sum = 0;
+    int highest = 0;
+    int lowest = Int32.MaxValue;
+    foreach (var v in fpsBuffer) {
+      sum += v;
+
+      if (fps > highest) {
+        highest = fps;
+      }
+      if (fps < lowest) {
+        lowest = fps;
+      }
+    }
+    FPSAverage = sum / frameRange;
+    FPSHighest = highest;
+    FPSLowest = lowest;
+  }
+}
---
+++ tutorial/Assets/Scripts/FpsDisplay.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(FpsCounter))]
+public class FpsDisplay : MonoBehaviour {
+  [System.Serializable]
+  private struct FpsColor {
+    public Color color;
+    public int minimumFps;
+  }
+
+  public Text averageFpsLabel;
+  public Text highestFpsLabel;
+  public Text lowestFpsLabel;
+  [SerializeField]
+  private FpsColor[] fpsColors;
+
+  private FpsCounter counter;
+
+  void Awake() {
+    counter = GetComponent<FpsCounter>();
+  }
+
+  // Update is called once per frame
+  void Update() {
+    Display(averageFpsLabel, counter.FPSAverage);
+    Display(highestFpsLabel, counter.FPSHighest);
+    Display(lowestFpsLabel, counter.FPSLowest);
+  }
+
+  void Display(Text label, int fps) {
+    label.text = Mathf.Clamp(fps, 0, 99).ToString();
+    foreach(var v in fpsColors) {
+      if (fps >= v.minimumFps) {
+        label.color = v.color;
+        break;
+      }
+    }
+  }
+}
