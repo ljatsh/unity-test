34c98d59caa566684abffa66c9fd230c727f7744
lj@sh <longjun.wang@qq.com>
2019-09-24 08:50:22 +0800

    unity - check that  Resources/AssetDatabase/AssetBundle.LoadAsset is cleaver enough

--- tutorial/Assets/Tests/UnityAsset.cs
+++ tutorial/Assets/Tests/UnityAsset.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
-using UnityEngine.TestTools;
+using UnityEditor;
 
 // Tips
 // https://docs.unity3d.com/ScriptReference/Resources.html
@@ -19,7 +19,7 @@ namespace Tests
       Texture2D tex = Resources.Load<Texture2D>("heroIcon_10171");
       Assert.That(obj, Is.Not.Null);
       Assert.That(tex, Is.Not.Null);
-      Assert.That(obj == tex, Is.True);
+      Assert.That(obj == tex, Is.True, "实际类型是确定的");
 
       // 路径去掉Resources和extension
       Assert.That(Resources.Load("Assets/Tests/Resources/heroIcon_10171.png"), Is.Null);
@@ -42,6 +42,17 @@ namespace Tests
     public void TestResourceUnload()
     {
       // TODO
+    }
+
+    [Test]
+    public void TestAssetDatabase()
+    {
+      Object obj1 = AssetDatabase.LoadAssetAtPath("Assets/Tests/heroIcon_10181.png", typeof(Object));
+      Assert.That(obj1, Is.Not.Null);
+      obj1.name = "AssetDatabaseTest";
+      Object obj2 = AssetDatabase.LoadAssetAtPath("Assets/Tests/heroIcon_10181.png", typeof(Object));
+      Assert.That(System.Object.ReferenceEquals(obj1, obj2), "Unity的Object不会重复创建，除非bundle模式下接触关系");
+      Assert.That(obj2.name, Is.EqualTo("AssetDatabaseTest"));
     }
   }
 }
