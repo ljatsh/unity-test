090f232c5bdb296853b0540219d9abfda2f03f0a
lj@sh <longjun.wang@qq.com>
2019-10-22 11:44:36 +0800

    charep - regex replacement

--- tutorial/Assets/Tests/RegexTest.cs
+++ tutorial/Assets/Tests/RegexTest.cs
@@ -19,6 +19,7 @@ using NUnit.Framework;
 
 // TODO:
 // 1. \b{ name }
+// 2. 重复的group， 包括带名字的group
 
 namespace CSharp {
   public class RegexTest {
@@ -84,21 +85,19 @@ namespace CSharp {
     }
 
     [Test]
-    public void TestCaptureTODO() {
+    public void TestCaptureGroup() {
       string input = "Yes.Hello. This dog is very friendly.";
       string pattern = @"(\w+\s*)+\.";
-      foreach (Match match in Regex.Matches(input, pattern)) {
-
-
-         UnityEngine.Debug.LogFormat("Match: {0}", match.Value);
-         for (int groupCtr = 0; groupCtr < match.Groups.Count; groupCtr++) {
-            Group group = match.Groups[groupCtr];
-            UnityEngine.Debug.LogFormat("   Group {0}: {1}", groupCtr, group.Value);
-            // for (int captureCtr = 0; captureCtr < group.Captures.Count; captureCtr++)
-            //    UnityEngine.Debug.LogFormat("      Capture {0}: {1}", captureCtr, 
-            //                      group.Captures[captureCtr].Value);
-         }                      
-      }
+      // foreach (Match match in Regex.Matches(input, pattern)) {
+      //    UnityEngine.Debug.LogFormat("Match: {0}", match.Value);
+      //    for (int groupCtr = 0; groupCtr < match.Groups.Count; groupCtr++) {
+      //       Group group = match.Groups[groupCtr];
+      //       UnityEngine.Debug.LogFormat("   Group {0}: {1}", groupCtr, group.Value);
+      //       for (int captureCtr = 0; captureCtr < group.Captures.Count; captureCtr++)
+      //          UnityEngine.Debug.LogFormat("      Capture {0}: {1}", captureCtr, 
+      //                            group.Captures[captureCtr].Value);
+      //    }                      
+      // }
 
       MatchCollection c = Regex.Matches(input, pattern);
       Assert.That(c.Count, Is.EqualTo(3));
@@ -111,9 +110,105 @@ namespace CSharp {
         GroupCollection gc = m.Groups;
         Assert.That(gc.Count, Is.EqualTo(2));
 
+        Group g = gc[0];
+        Assert.That(g.Value, Is.EqualTo("Yes."), "第一个group是match本身");
+
+          // Capture
+          CaptureCollection cc = g.Captures;
+          Assert.That(cc.Count, Is.EqualTo(1));
+          Assert.That(cc[0].Value, Is.EqualTo("Yes."));
+
+        g = gc[1];
+        Assert.That(g.Value, Is.EqualTo("Yes"));
+
+          // Capture
+          cc = g.Captures;
+          Assert.That(cc.Count, Is.EqualTo(1));
+          Assert.That(cc[0].Value, Is.EqualTo("Yes"));
+
       // Match 2
+      m = c[1];
+      Assert.That(m.Value, Is.EqualTo("Hello."));
+
+        // Group
+        gc = m.Groups;
+        Assert.That(gc.Count, Is.EqualTo(2));
+
+        g = gc[0];
+        Assert.That(g.Value, Is.EqualTo("Hello."));
+
+          // Capture
+          cc = g.Captures;
+          Assert.That(cc.Count, Is.EqualTo(1));
+          Assert.That(cc[0].Value, Is.EqualTo("Hello."));
+
+        // Group
+        g = gc[1];
+        Assert.That(g.Value, Is.EqualTo("Hello"));
+
+          // Capture
+          cc = g.Captures;
+          Assert.That(cc.Count, Is.EqualTo(1));
+          Assert.That(cc[0].Value, Is.EqualTo("Hello"));
 
       // Match 3
+      m = c[2];
+      Assert.That(m.Value, Is.EqualTo("This dog is very friendly."));
+
+        // Group
+        gc = m.Groups;
+        Assert.That(gc.Count, Is.EqualTo(2));
+
+        g = gc[0];
+        Assert.That(g.Value, Is.EqualTo("This dog is very friendly."));
+
+          // Capture
+          cc = g.Captures;
+          Assert.That(cc.Count, Is.EqualTo(1));
+          Assert.That(cc[0].Value, Is.EqualTo("This dog is very friendly."));
+
+        // Group
+        g = gc[1];
+        Assert.That(g.Value, Is.EqualTo("friendly"), "重复的group只会保留最后匹配的值");
+
+          // Capture
+          cc = g.Captures;
+          Assert.That(cc.Count, Is.EqualTo(5));
+          Assert.That(cc[0].Value, Is.EqualTo("This "));
+          Assert.That(cc[1].Value, Is.EqualTo("dog "));
+          Assert.That(cc[2].Value, Is.EqualTo("is "));
+          Assert.That(cc[3].Value, Is.EqualTo("very "));
+          Assert.That(cc[4].Value, Is.EqualTo("friendly"));
+    }
+
+    [Test]
+    public void TestReplacementString() {
+      // simple string replacement
+      string ret = Regex.Replace("Can you help $name? Monsters are attacking $name", @"\$\w+", "ljatsh");
+      Assert.That(ret, Is.EqualTo("Can you help ljatsh? Monsters are attacking ljatsh"));
+
+      // capture replacement
+      ret = Regex.Replace("name =ljatsh, age =   33 !", @"(\w+)\s*=\s*(\w+)", "$2 = $1");
+      Assert.That(ret, Is.EqualTo("ljatsh = name, 33 = age !"));
+    }
+
+    [Test]
+    public void TestReplacementDelegate() {
+      Dictionary<string, string> kv = new Dictionary<string, string>() {
+        {"name", "ljatsh"},
+        {"age", "35"},
+        {"sex", "man"}
+      };
+
+      string ret = Regex.Replace("owner = $name, age = $age, sex = $sex, like = $likes", @"(?<left>\w+)\s*=\s*\$(?<key>\w+)", (Match m) => {
+        string v;
+        string key = m.Groups["key"].Value;
+        if (kv.TryGetValue(key, out v)) {
+          return string.Format("{0} = {1}", m.Groups["left"].Value, v);
+        }
+        return m.Value;
+      });
+      Assert.That(ret, Is.EqualTo("owner = ljatsh, age = 35, sex = man, like = $likes"));
     }
 
     private string GetMatchesChars(Regex pattern) {
