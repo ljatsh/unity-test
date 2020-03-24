87e32223075674d5a2ff1d7b047d59d6ea62f66a
lj@sh <longjun.wang@qq.com>
2020-03-24 13:53:57 +0800

    refine the clock sample to help Peter

--- tutorial/Assets/Scripts/Clock.cs
+++ tutorial/Assets/Scripts/Clock.cs
@@ -10,9 +10,15 @@ public class Clock : MonoBehaviour {
 
   public Boolean isUpdateContinous;
 
+  public Boolean paused = false;
+
+  public float speed = 1.0f;
+
+  private DateTime startTime;
+
   // Start is called before the first frame update
   void Start() {
-    
+    startTime = DateTime.Now;
   }
 
   // Update is called once per frame
@@ -25,14 +31,21 @@ public class Clock : MonoBehaviour {
   }
 
   void updateDiscrete() {
-    DateTime now = DateTime.Now;
+    if (paused)
+      return;
+
+    DateTime now = startTime + TimeSpan.FromSeconds(Time.time * speed);
     hourArm.rotation = Quaternion.Euler(0f, (now.Hour + now.Minute / 60.0f) * 30.0f, 0f);
     minuteArm.rotation = Quaternion.Euler(0f, now.Minute * 6.0f, 0f);
     secondArm.rotation = Quaternion.Euler(0f, now.Second * 6.0f, 0f);
   }
 
   void updateContinous() {
-    TimeSpan span = DateTime.Now.TimeOfDay;
+    if (paused)
+      return;
+
+    DateTime now = startTime + TimeSpan.FromSeconds(Time.time * speed);
+    TimeSpan span = now.TimeOfDay;
     hourArm.rotation = Quaternion.Euler(0f, (float)(span.TotalHours * 30.0f), 0f);
     minuteArm.rotation = Quaternion.Euler(0f, (float)(span.TotalMinutes * 6.0f), 0f);
     secondArm.rotation = Quaternion.Euler(0f, (float)(span.TotalSeconds * 6.0f), 0f);
